f9168665d34c0aa15b5ab508bf8d9b3d76c9f1ed
atbriansedano <atbriansedano@gmail.com>
2023-08-17 20:17:00 -0500

    Version 1.2

--- file-automation.py
+++ file-automation.py
@@ -8,98 +8,90 @@ Usage: Run the .exe file to manage new files outside of the folder structure
 
 Author: Brian Sedano
 Date: 16/08/2023
-Version: 1.0
+Version: 1.2
 Dependencies: OS, Shuttle.
 """
-#!/usr/bin/env python3
+
 import os
 import shutil
 
-##########################################################################################################################
+###################################
+#       Folder Creation           #
+###################################
 
-def folderPathCheck(strRootFolderPath):
-    print("Create folder paths")
+def folderCheck(lstFolders,strRootPath):
     
-    lstFolders = ['Document','Image','Audio','Video','Exe','Zip','Folder']
-
     for x in lstFolders:
-        if os.path.exists(strRootFolderPath+x):
-            print("Folder path {} exists".format(x))
+        if os.path.exists(strRootPath+x):
+           print("Folder path {} exists".format(x))
         else:
             print("Folder path {} does not exist. Creating folder path...".format(x))
-            os.makedirs(strRootFolderPath+x)
+            os.makedirs(strRootPath+x)
 
-##########################################################################################################################
+###################################
+#              Main               #
+###################################
 
+#Folder path initialization
+strDesktopRoot = os.path.expanduser('~/Desktop/')
+strDocRoot = os.path.expanduser('~/Documents/')
+strDownloadRoot = os.path.expanduser('~/Downloads/')
+strPicRoot = os.path.expanduser('~/Pictures/')
+strMusicRoot = os.path.expanduser('~/Music/')
+strVideoRoot = os.path.expanduser('~/Videos/')
 
-def moveFiles(strDirPath, lstDirFiles):
-    print("move file")
+lstRootPaths = [strDocRoot,strDownloadRoot,strPicRoot,strMusicRoot,strVideoRoot]
 
-    StrDocRoot = os.path.expanduser('~/Documents')
-    strDocPath = StrDocRoot+'/'+'Automation'+'/'
+#Folder Lists
+lstDocFolders = ['Word', 'Excel', 'PowerPoint', 'PDF', 'Text File', 'Executables', 'Zip','Other']
+lstPictureFolders = ['JPG', 'JPEG', 'PNG', 'GIF', 'BMP']
+lstAudioFolders = ['MP3','WAV','Other']
+lstVideoFolders = ['MP4','AVI','Other']
 
-    lstDocExt = ['.docx','.doc','.xlsx','.xls','pptx','.ppt','.pdf','.txt','.csv','.rtf',
-                 '.html','.xml','.json','.md','.psd','.ai','.svg','.webp','.htm','.pages']
-    lstImgExt = ['.jpg','.jpeg','.png','.gif','.bmp']
-    lstAudioExt = ['.mp3','.wav','.mp4','.mov','.avi','.avi','m4a']
-    lstVideoExt = ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.3gp', 
-                   '.mpg', '.mpeg', '.m4v', '.ts', '.vob', '.rm', '.rmvb', '.asf', '.ogv']
-    lstExeExt = ['.exe','.bat','.cmd','.com','.ps1','.vbs','.js','.jar','.py','.sh','.msi','.vs','.c']
-    lstZipExt = ['.zip','.rar','.7z','.gz','.tar.gz']
+#Checking/Creating file structure
+folderCheck(lstDocFolders,strDocRoot)
+folderCheck(lstPictureFolders,strPicRoot)
+folderCheck(lstAudioFolders,strMusicRoot)
+folderCheck(lstVideoFolders,strVideoRoot)
 
-    #print ("Moving these files: {}".format(lstDirFiles))
-    for x in lstDirFiles:
-        try:
-            if any(y in x.lower() for y in lstDocExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Document'+'/')
-            if any(y in x.lower() for y in lstImgExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Image'+'/')
-            if any(y in x.lower() for y in lstAudioExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Audio'+'/')
-            if any(y in x.lower() for y in lstVideoExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Video'+'/')
-            if any(y in x.lower() for y in lstExeExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Exe'+'/')
-            if any(y in x.lower() for y in lstZipExt):
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Zip'+'/')
-            if 'Automation' not in x and '.ini' not in x and 'GitHub' not in x:
-                shutil.move(strDirPath+'/'+x, strDocPath+'/'+'Folder'+'/')
-        except Exception as e:
-            print('File not found or alreadyn moved')
+#Extention lists
+dictDocument = {
+    'Path': strDocRoot,
+    'Word': ['.docx','.doc'],
+    'Excel': ['.xlsx','.xls'],
+    'PowerPoint': ['.pptx','.ppt'],
+    'PDF': ['.pdf'],
+    'Text File': ['.txt','.csv','.rtf','.html','.xml','.json','.md','.psd','.ai','.svg','.htm','.pages','.css',],
+    'Executables': ['.exe','.bat','.cmd','.com','.ps1','.vbs','.js','.jar','.py','.sh','.msi','.vs','.c','.s'],
+    'Zip': ['.zip','.rar','.7z','.gz','.tar.gz'],
+    'Other': ['.webp']
+}
 
-##########################################################################################################################
+dictPictures = {'Path': strPicRoot,'JPG': ['.jpg'], 'JPEG': ['.jpeg'], 'PNG': ['.png'], 'GIF': ['.gif'], 'Other': ['.bmp']}
+dictAudio = {'Path': strMusicRoot,'MP3': ['.mp3'], 'WAV': ['.wav'], 'Other': ['.m4a']}
+dictVideo = {'Path': strVideoRoot,'MP4': ['.mp4'], 'AVI': ['.avi'], 'Other': ['.mkv', '.mov', '.wmv', '.flv', '.webm', '.3gp', 
+                '.mpg', '.mpeg', '.m4v', '.ts', '.vob', '.rm', '.rmvb', '.asf', '.ogv']}
 
-def fileCheck(strDocPath, strDownloadPath):
-    print('Check directory files')
+lstAllDict = [dictDocument,dictPictures,dictAudio,dictVideo]
 
-    lstDocDirFiles = os.listdir(strDocPath)
-    lstDownloadDirFiles = os.listdir(strDownloadPath)
-    
-    #print(lstDocDirFiles)
+###################################
+#         File Managment          #
+###################################
 
-    for x in lstDownloadDirFiles:
-          if x != 'Automation' and x != ' ':
-            boolFileExists = True
-            if boolFileExists == True:
-                moveFiles(strDownloadPath,  lstDownloadDirFiles)
-                break
-
-    for x in lstDocDirFiles:
-        if x != 'Automation' and x != ' ':
-            boolFileExists = True
-            if boolFileExists == True:
-                moveFiles(strDocPath,  lstDocDirFiles)
-                break
-    
-##########################################################################################################################
-
-
-   
-StrDocRoot = os.path.expanduser('~/Documents')
-StrDownloadRoot = os.path.expanduser('~/Downloads')
-
-oStrDocPath = StrDocRoot+'/'+'Automation'+'/'
-
-folderPathCheck(oStrDocPath)
-fileCheck(StrDocRoot, StrDownloadRoot)
-
+#Checking all paths
+for x in lstRootPaths:
+    lstAllFiles = os.listdir(x)
+    #Checking all files
+    for y in lstAllFiles:
+        strExt = y[y.rfind('.'):].lower() 
+        if len(strExt) >= 2 or len(strExt) >= 4:
+            #Checking all Dictionaries
+            for z in lstAllDict:
+                #All values inside Dictionaries
+                for key,value in z.items():
+                    #If extention in values of a key, return a key
+                    if  strExt in value:
+                        print("Moving file {}".format(y))
+                        shutil.move(x+y,str(z['Path'])+key)
+        else:
+            print('File entry is folder. Cannot move')
